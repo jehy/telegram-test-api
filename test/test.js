da7f5cedd8ab2c1b1a4c2e0e5f8a22879208da4b
Jehy <fate@jehy.ru>
2019-03-04 15:56:38 +0300

    add test for getUpdatesHistory, refactor tests

--- test/test.js
+++ test/test.js
@@ -89,15 +89,14 @@ class DeleterBot extends TelegramBot {
   }
 }
 
-const assertEventuallyTrue = (timeoutDuration, message, func) => {
+async function assertEventuallyTrue(timeoutDuration, message, func) {
   if (func()) {
-    return Promise.resolve();
+    return true;
   }
-
   return Promise.delay(10)
     .then(() => assertEventuallyTrue(timeoutDuration, message, func))
     .timeout(timeoutDuration, message);
-};
+}
 
 describe('Telegram Server', () => {
   const serverConfig = {port: 9001};
@@ -119,150 +118,147 @@ describe('Telegram Server', () => {
     return server.stop();
   });
 
-  it('should receive user`s messages', function sendClientMessages() {
+  it('should receive user`s messages', async function sendClientMessages() {
     this.slow(200);
     this.timeout(1000);
     const message = client.makeMessage('/start');
-    return client.sendMessage(message)
-      .then(res=>assert.equal(true, res.ok));
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
   });
 
-  it('should provide user messages to bot', function testGetUserMessages() {
+  it('should provide user messages to bot', async function testGetUserMessages() {
     this.slow(200);
     this.timeout(1000);
     const message = client.makeMessage('/start');
-    let telegramBot;
-    return client.sendMessage(message)
-      .then((res) => {
-        assert.equal(true, res.ok);
-        const botOptions = {polling: true, baseApiUrl: server.ApiURL};
-        telegramBot = new TelegramBotEx(token, botOptions);
-        return telegramBot.waitForReceiveUpdate();
-      }).then((res) => {
-        assert.equal('/start', res.text);
-        debug('Stopping polling');
-        return telegramBot.stopPolling();
-      })
-      .then(() => {
-        debug('Polling stopped');
-      });
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
+    const botOptions = {polling: true, baseApiUrl: server.ApiURL};
+    const telegramBot = new TelegramBotEx(token, botOptions);
+    const res2 = await telegramBot.waitForReceiveUpdate();
+    assert.equal('/start', res2.text);
+    debug('Stopping polling');
+    await telegramBot.stopPolling();
+    debug('Polling stopped');
   });
 
-  it('should receive bot`s messages', function testBotReceiveMessages() {
+  it('should receive bot`s messages', async function testBotReceiveMessages() {
     this.slow(200);
     this.timeout(10000);
     const message = client.makeMessage('/start');
-    let telegramBot;
-    let testBot;
     const botWaiter = server.waitBotMessage();
-    return client.sendMessage(message)
-      .then((res) => {
-        assert.equal(true, res.ok);
-        const botOptions = {polling: true, baseApiUrl: server.ApiURL};
-        telegramBot = new TelegramBotEx(token, botOptions);
-        testBot = new TestBot(telegramBot);
-        return telegramBot.waitForReceiveUpdate();
-      }).then((res) => {
-        assert.equal('/start', res.text);
-        debug('Stopping polling');
-        return telegramBot.stopPolling();
-      })
-      .then(() => {
-        debug('Polling stopped');
-        return botWaiter;
-      })// wait until bot reply appears in storage
-      .then(() => {
-        Logger.botMessages(server.storage);
-        assert.equal(1, server.storage.botMessages.length, 'Message queue should contain one message!');
-        return true;
-      });
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
+    const botOptions = {polling: true, baseApiUrl: server.ApiURL};
+    const telegramBot = new TelegramBotEx(token, botOptions);
+    const testBot = new TestBot(telegramBot);
+    const res2 = await telegramBot.waitForReceiveUpdate();
+    assert.equal('/start', res2.text);
+    debug('Stopping polling');
+    await telegramBot.stopPolling();
+    debug('Polling stopped');
+    await botWaiter; // wait until bot reply appears in storage
+    Logger.botMessages(server.storage);
+    assert.equal(1, server.storage.botMessages.length, 'Message queue should contain one message!');
   });
 
 
-  it('should provide bot`s messages to client', function testClientGetUpdates() {
+  it('should provide bot`s messages to client', async function testClientGetUpdates() {
     this.slow(200);
     this.timeout(1000);
     const message = client.makeMessage('/start');
-    let telegramBot;
-    let testBot;
     const botWaiter = server.waitBotMessage();
-    return client.sendMessage(message)
-      .then((res) => {
-        assert.equal(true, res.ok);
-        const botOptions = {polling: true, baseApiUrl: server.ApiURL};
-        telegramBot = new TelegramBotEx(token, botOptions);
-        testBot = new TestBot(telegramBot);
-        return telegramBot.waitForReceiveUpdate();
-      }).then((res) => {
-        assert.equal('/start', res.text);
-        debug('Stopping polling');
-        return telegramBot.stopPolling();
-      })
-      .then(() => {
-        debug('Polling stopped');
-        return botWaiter;
-      })// wait until bot reply appears in storage
-      .then(() => client.getUpdates())
-      .then((updates) => {
-        Logger.serverUpdate(updates.result);
-        assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
-        return true;
-      });
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
+    const botOptions = {polling: true, baseApiUrl: server.ApiURL};
+    const telegramBot = new TelegramBotEx(token, botOptions);
+    const testBot = new TestBot(telegramBot);
+    const res2 = await telegramBot.waitForReceiveUpdate();
+    assert.equal('/start', res2.text);
+    debug('Stopping polling');
+    await telegramBot.stopPolling();
+    debug('Polling stopped');
+    await botWaiter;
+    const updates = await client.getUpdates();
+    Logger.serverUpdate(updates.result);
+    assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
   });
 
 
-  it('should fully implement user-bot interaction', function testFull() {
+  it('should fully implement user-bot interaction', async function testFull() {
     this.slow(400);
     this.timeout(1000);
     let message = client.makeMessage('/start');
-    let telegramBot;
-    let testBot;
-    return client.sendMessage(message)
-      .then((res) => {
-        assert.equal(true, res.ok);
-        const botOptions = {polling: true, baseApiUrl: server.ApiURL};
-        telegramBot = new TelegramBotEx(token, botOptions);
-        testBot = new TestBot(telegramBot);
-        return client.getUpdates();
-      })
-      .then((updates) => {
-        Logger.serverUpdate(updates.result);
-        assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
-        const keyboard = JSON.parse(updates.result[0].message.reply_markup).keyboard;
-        message = client.makeMessage(keyboard[0][0].text);
-        client.sendMessage(message);
-        return client.getUpdates();
-      })
-      .then((updates) => {
-        Logger.serverUpdate(updates.result);
-        assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
-        assert.equal('Hello, Masha!', updates.result[0].message.text, 'Wrong greeting message!');
-        debug('Stopping polling');
-        return telegramBot.stopPolling();
-      })
-      .then(() => {
-        debug('Polling stopped');
-        return true;
-      });
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
+    const botOptions = {polling: true, baseApiUrl: server.ApiURL};
+    const telegramBot = new TelegramBotEx(token, botOptions);
+    const testBot = new TestBot(telegramBot);
+    const updates = await client.getUpdates();
+    Logger.serverUpdate(updates.result);
+    assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
+    const {keyboard} = JSON.parse(updates.result[0].message.reply_markup);
+    message = client.makeMessage(keyboard[0][0].text);
+    await client.sendMessage(message);
+    const updates2 = await client.getUpdates();
+    Logger.serverUpdate(updates2.result);
+    assert.equal(1, updates2.result.length, 'Updates queue should contain one message!');
+    assert.equal('Hello, Masha!', updates2.result[0].message.text, 'Wrong greeting message!');
+    debug('Stopping polling');
+    await telegramBot.stopPolling();
+    debug('Polling stopped');
   });
 
-  it('should allow messages deletion', function () {
+
+  it('should get updates history', async function testUpdatesHistory() {
+    this.slow(400);
+    this.timeout(1000);
+    let message = client.makeMessage('/start');
+    const res = await client.sendMessage(message);
+    assert.equal(true, res.ok);
+    const botOptions = {polling: true, baseApiUrl: server.ApiURL};
+    const telegramBot = new TelegramBotEx(token, botOptions);
+    const testBot = new TestBot(telegramBot);
+    const updates = await client.getUpdates();
+    Logger.serverUpdate(updates.result);
+    assert.equal(1, updates.result.length, 'Updates queue should contain one message!');
+    const {keyboard} = JSON.parse(updates.result[0].message.reply_markup);
+    message = client.makeMessage(keyboard[0][0].text);
+    await client.sendMessage(message);
+    const updates2 = await client.getUpdates();
+    Logger.serverUpdate(updates2.result);
+    assert.equal(1, updates2.result.length, 'Updates queue should contain one message!');
+    assert.equal('Hello, Masha!', updates2.result[0].message.text, 'Wrong greeting message!');
+
+    const history = await client.getUpdatesHistory();
+    assert.equal(history.length, 4);
+    history.forEach((item, index)=>{
+      assert.ok(item.time);
+      assert.ok(item.botToken);
+      assert.ok(item.message);
+      assert.ok(item.updateId);
+      assert.ok(item.messageId);
+      if (index > 0) {
+        assert.isAbove(item.time, history[index - 1].time);
+      }
+    });
+    debug('Stopping polling');
+    await telegramBot.stopPolling();
+    debug('Polling stopped');
+  });
+
+  it('should allow messages deletion', async function () {
     this.slow(400);
     this.timeout(1000);
     const botOptions = {polling: true, baseApiUrl: server.ApiURL};
     const unusedTelegramBot = new DeleterBot(token, botOptions);
     let message = client.makeMessage('delete'); // Should be deleted
-    return client.sendMessage(message)
-      .then((res) => {
-        assert.ok(res.ok);
-        message = client.makeMessage('keep safe'); // Shouldn't be deleted
-        return client.sendMessage(message);
-      })
-      .then((res) => {
-        assert.ok(res.ok);
-        return assertEventuallyTrue(500, 'User messages count should become 1', () => (
-          server.storage.userMessages.length === 1
-        ));
-      });
+    const res = await client.sendMessage(message);
+    assert.ok(res.ok);
+    message = client.makeMessage('keep safe'); // Shouldn't be deleted
+    const res2 = await client.sendMessage(message);
+    assert.ok(res2.ok);
+    return assertEventuallyTrue(500, 'User messages count should become 1', () => (
+      server.storage.userMessages.length === 1
+    ));
   });
 });
