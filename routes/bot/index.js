24b7a553f79d81b6a8807cef8c9fd30fb3c3e95b
Pavel Shakhov <ab.pongo@gmail.com>
2021-01-25 14:27:29 +0300

    sendChatAction

--- routes/bot/index.js
+++ routes/bot/index.js
@@ -7,6 +7,7 @@ module.exports = [
   require('./getUpdates'),
   require('./getMe'),
   require('./sendMessage'),
+  require('./sendChatAction'),
   require('./sendPhoto'),
   require('./deleteWebhook'),
   require('./unknownMethod'), // This route should go after all bot API methods.
