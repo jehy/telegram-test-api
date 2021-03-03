42b4f8aa0b3b82c1bbc823fd6c107fd68850e7cd
Vladimir Chizhov <mirlord@protonmail.ch>
2021-03-03 09:06:56 -0800

    Support webhooks

--- routes/bot/deleteWebhook.js
+++ routes/bot/deleteWebhook.js
@@ -4,6 +4,8 @@ const {handle} = require('./utils');
 
 const deleteWebhook = (app, telegramServer)=> {
   handle(app, '/bot:token/deleteWebhook', (req, res, unusedNext) => {
+    const botToken = req.params.token;
+    telegramServer.deleteWebhook(botToken);
     const data = {ok: true, result: true, description: 'Webhook was deleted'};
     res.sendResult(data);
   });
