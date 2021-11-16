45ed91419b2237a4571acbea8735b09cd17dec02
Jehy <fate@jehy.ru>
2021-11-16 17:53:14 +0300

    fix standalone launch

--- bin/start.js
+++ bin/start.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const TelegramServer = require('../telegramServer');
+const TelegramServer = require('../lib/index');
 // eslint-disable-next-line import/no-unresolved
 const config = require('../config/config.json');
 
